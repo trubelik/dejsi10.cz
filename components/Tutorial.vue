59e0a831927b12c184c6aa830e9399f7fde63838
Martin Trubelik <martin.trubelik@oracle.com>
2023-01-29 13:49:27 +0100

    Deployment

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
     <h1 class="text-5xl font-bold underline">
-      Hello world!
+      Hello world! Test
     </h1>
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
       <a class="flex justify-center pt-8 sm:pt-0" href="https://nuxtjs.org" target="_blank">
