6efaf2915cfa968942087214d50ce7f0a7a97e40
Martin Trubelik <martin.trubelik@oracle.com>
2023-02-08 02:00:08 +0100

    Adding responsive menu

--- components/Header.vue
+++ components/Header.vue
@@ -1,23 +1,36 @@
 <template>
   <Body>
-    <div class="py-5 px-2 flex">
+    <nav class="py-5 px-2 flex flex-wrap items-center justify-between">
       <NuxtLink to="/" class="text-xl font-bold">
         Dejsi10
       </NuxtLink>
-      <nav class="ml-auto">
-        <NuxtLink to="/">
+      <button type="button" class="inline-flex items-center p-2 text-gray-700 rounded-lg md:hidden hover:bg-gray-100 text-xl" @click="hide = !hide">
+        <font-awesome-icon icon="bars" />
+      </button>
+      <div :class="{'hidden': hide, 'flex': !hide, 'main-navigation': true}">
+        <NuxtLink to="/" class="main-page">
           Hlavní stránka
         </NuxtLink>
         <NuxtLink to="/photos">
           Fotografie
         </NuxtLink>
-      </nav>
-    </div>
+      </div>
+    </nav>
   </Body>
 </template>
 
 <script>
 export default {
-  name: 'NuxtHeader'
+  name: 'NuxtHeader',
+  data () {
+    return {
+      hide: true
+    }
+  },
+  watch: {
+    $route (to, from) {
+      if (to !== from) { this.hide = true }
+    }
+  }
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,8 @@ export default {
         'faChevronRight',
         'faTree',
         'faHouse',
-        'faDownload'
+        'faDownload',
+        'faBars'
       ]
     }
   },
