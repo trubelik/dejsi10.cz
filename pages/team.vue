e964befd3e9434b7eceb6ac16e8a9db17aad6094
Martin Trubelik <martin.trubelik@oracle.com>
2023-02-17 02:08:24 +0100

    Updating team for better responsive changes

--- pages/team.vue
+++ pages/team.vue
@@ -12,9 +12,9 @@
       <div
         v-for="(teamMember, index) in teamPage.teamMembers"
         :key="'member_' + index"
-        class="bg-white rounded-2xl p-6 md:px-10 md:py-8 mt-40 relative"
+        class="relative pt-40"
       >
-        <div class="-top-40 left-0 w-full absolute" style="height: 200px;">
+        <div class="top-0 left-0 w-full absolute" style="height: 200px;">
           <nuxt-img
             class="rounded-xl mx-auto shadow-xl max-w-full max-h-full"
             :src="teamMember.photo.url"
@@ -23,19 +23,21 @@
             format="webp"
           />
         </div>
-        <h1 class="mt-8 mb-3 text-xl sm:text-2xl tracking-tight text-slate-900 font-extrabold">
-          {{ teamMember.name }}
-        </h1>
-        <p>
-          {{ teamMember.description }}
-        </p>
-        <div v-if="teamMember.motto" class="text-center mt-3">
-          <div>
-            <font-awesome-icon icon="quote-left" class="text-4xl text-amber-200" />
+        <div class="bg-white rounded-2xl p-6 md:px-10 md:py-8">
+          <h1 class="mt-8 mb-3 text-xl sm:text-2xl tracking-tight text-slate-900 font-extrabold">
+            {{ teamMember.name }}
+          </h1>
+          <p>
+            {{ teamMember.description }}
+          </p>
+          <div v-if="teamMember.motto" class="text-center mt-3">
+            <div>
+              <font-awesome-icon icon="quote-left" class="text-4xl text-amber-200" />
+            </div>
+            <blockquote class="text-xl italic">
+              "{{ teamMember.motto }}"
+            </blockquote>
           </div>
-          <blockquote class="text-xl italic">
-            "{{ teamMember.motto }}"
-          </blockquote>
         </div>
       </div>
     </div>
