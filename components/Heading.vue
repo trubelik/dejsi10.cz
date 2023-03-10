60e63f2d5235c5aa65f2fb7d23a97ebb4d5a7c92
Martin Trubelik <martin.trubelik@oracle.com>
2023-03-10 16:22:41 +0100

    Create Heading.vue

---
+++ components/Heading.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1 :class="classes">
+    <slot />
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'NuxtHeading',
+  props: {
+    variant: {
+      type: String,
+      default: 'normal',
+      validator (value) {
+        // The value must match one of these strings
+        return ['normal', 'big', 'small'].includes(value)
+      }
+    }
+  },
+  data () {
+    return {
+      classes: {
+        'text-3xl': this.variant === 'normal',
+        'sm:text-4xl': this.variant === 'normal',
+        'tracking-tight': true,
+        'text-slate-900': true,
+        'font-extrabold': true,
+        'text-4xl': this.variant === 'big',
+        'sm:text-5xl': this.variant === 'big',
+        'text-xl': this.variant === 'small',
+        'sm:text-2xl': this.variant === 'small'
+      }
+    }
+  }
+}
+</script>
