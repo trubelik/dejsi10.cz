3ddbbf5aa9faea69da7c8ff3eeee77b719f99bfc
Martin Trubelik <martin.trubelik@oracle.com>
2023-03-10 16:22:27 +0100

    Two sizes of body component

--- components/Body.vue
+++ components/Body.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="xl:max-w-screen-lg mx-auto max-w-screen-md">
-    <slot />
+  <div :class="classes">
+    <section>
+      <slot />
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'NuxtBody'
+  name: 'NuxtBody',
+  props: {
+    wide: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      classes: {
+        'xl:max-w-screen-lg mx-auto max-w-screen-md': !this.wide,
+        'xl:max-w-screen-xl mx-auto max-w-screen-lg': this.wide
+      }
+    }
+  }
 }
 </script>
